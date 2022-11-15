813eb38c4dc76948723a891c3342029cd5f2eb11
BOT dqy <278987638@qq.com>
2022-11-15 09:25:14 +0800

    update

--- VariationalRecurrentNeuralNetwork-master/train.py
+++ VariationalRecurrentNeuralNetwork-master/train.py
@@ -14,14 +14,16 @@ using unimodal isotropic gaussian distributions for
 inference, prior, and generating models."""
 
 
-def train(epoch):
+def train(epoch, device):
+	model.train()
 	train_loss = 0
 	for batch_idx, (data, _) in enumerate(train_loader):
 		# data:[batch_size, num_channels, H, W]
 
-		data = data.squeeze().transpose(0, 1) # (H, batch_size, W), where H is treated as time_steps. Here num_channels=1 and would be squeezed
+		# Initialization
+		data = data.squeeze().transpose(0, 1) # (H, batch_size, W), where H is treated as time_steps, W is treated as x_dim. Here num_channels=1 and would be squeezed
 		data = (data - data.min()) / (data.max() - data.min())
-		
+		data.to(device)
 		# forward + backward + optimize
 		optimizer.zero_grad()
 		kld_loss, nll_loss, _, _ = model(data)
@@ -32,14 +34,14 @@ def train(epoch):
 		
 		# printing
 		if batch_idx % print_every == 0:
-			print('Train Epoch: {} [{}/{} ({:.0f}%)]\t KLD Loss: {:.6f} \t NLL Loss: {:.6f}'.format(
+			print('Train Epoch: {} [{}/{} ({:.0f}%)]\t KLD Loss: {:.6f} \t Rec Loss: {:.6f}'.format(
 				epoch, batch_idx * len(data), len(train_loader.dataset),
 				100. * batch_idx / len(train_loader),
 				kld_loss / batch_size,
 				nll_loss / batch_size))
 
 			sample = model.sample(28)
-			plt.imshow(sample.numpy())
+			plt.imshow(sample.numpy(), cmap='gray')
 			plt.pause(1e-6)
 
 		train_loss += loss
@@ -48,61 +50,70 @@ def train(epoch):
 		epoch, train_loss / len(train_loader.dataset)))
 
 
-def test(epoch):
+def test(epoch, device):
 	"""uses test data to evaluate 
 	likelihood of the model"""
-	
+	model.eval()
 	mean_kld_loss, mean_nll_loss = 0, 0
-	for i, (data, _) in enumerate(test_loader):                                            
-		data = data.squeeze().transpose(0, 1)
-		data = (data - data.min()) / (data.max() - data.min())
 
-		kld_loss, nll_loss, _, _ = model(data)
-		mean_kld_loss += kld_loss
-		mean_nll_loss += nll_loss
+	with torch.no_grad():
+		for i, (data, _) in enumerate(test_loader):                                            
+			data = data.squeeze().transpose(0, 1)
+			data = (data - data.min()) / (data.max() - data.min())
+			data.to(device)
+			kld_loss, nll_loss, _, _ = model(data)
+			mean_kld_loss += kld_loss
+			mean_nll_loss += nll_loss
 
 	mean_kld_loss /= len(test_loader.dataset)
 	mean_nll_loss /= len(test_loader.dataset)
 
-	print('====> Test set loss: KLD Loss = {:.4f}, NLL Loss = {:.4f} '.format(
+	print('====> Test set loss: KLD Loss = {:.4f}, Rec Loss = {:.4f} '.format(
 		mean_kld_loss, mean_nll_loss))
 
 
-#hyperparameters
-x_dim = 28
-h_dim = 100
+
+# Hyperparameters
+x_dim = 28		
+h_dim = 100		
 z_dim = 16
-n_layers =  1
+n_layers =  1 	# Number of layers in hidden states
 n_epochs = 100
-clip = 10
+clip = 10		# Gradient clip
 learning_rate = 1e-3
 batch_size = 128
+device = "cpu"
+
 seed = 128
 print_every = 100
 save_every = 10
 
-#manual seed
+# Manual seed
 torch.manual_seed(seed)
 plt.ion()
 
-#init model + optimizer + datasets
+# GPU
+if torch.cuda.is_available(): device = "cuda"
+kwargs = {"num_workers":2, "pin_memory":True} if device =='cuda' else {} # Training Settings
+
+# Init model + optimizer + datasets
 train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('data', train=True, download=False,
+    datasets.MNIST('data', train=True, download=True,
 		transform=transforms.ToTensor()),
-    batch_size=batch_size, shuffle=True)
+    batch_size=batch_size, shuffle=True, **kwargs)
 
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('data', train=False, 
 		transform=transforms.ToTensor()),
-    batch_size=batch_size, shuffle=True)
+    batch_size=batch_size, shuffle=True, **kwargs)
 
-model = VRNN(x_dim, h_dim, z_dim, n_layers)
+model = VRNN(x_dim, h_dim, z_dim, n_layers).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 if __name__ == "__main__":
 	for epoch in range(1, n_epochs + 1):
-		train(epoch)
-		test(epoch)
+		train(epoch, device=device)
+		test(epoch, device=device)
 
 		# #saving model
 		# if epoch % save_every == 1:
